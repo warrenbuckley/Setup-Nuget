73a3d27d9b731e56bd5ad448a1dbfd5dcd4bf5f9
Warren Buckley <warren@umbraco.com>
2019-08-16 14:11:25 +0100

    Update action yml & main.ts to download nuget.exe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as tc from '@actions/tool-cache';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    core.debug("Downloading Nuget tool");
+
+    // Download latest Nuget.exe
+    const nugetPath = await tc.downloadTool("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe");
+    core.debug(`Nuget file location ${nugetPath}`);
+
+    // Add Nuget.exe CLI tool to path for
+    // Other steps to be able to access it
+    core.addPath(nugetPath);
+
+    // Verify Nuget installed
+    exec.exec("nuget");
+
   } catch (error) {
     core.setFailed(error.message);
   }
