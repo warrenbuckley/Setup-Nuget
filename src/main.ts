a2ced331b846ecb738aed0345c3c17abd3380f63
Warren Buckley <warren@umbraco.com>
2019-08-17 20:18:34 +0100

    Need to rename file to append .exe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as tc from '@actions/tool-cache';
+import * as fs from 'fs';
 
 async function run() {
   try {
@@ -11,15 +12,19 @@ async function run() {
     const nugetPath = await tc.downloadTool("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe");
     core.debug(`Nuget file location ${nugetPath}`);
 
+    // TODO Path join?!
+    // So we get a nuget.exe file not GUID.exe
+    fs.renameSync(nugetPath, `${nugetPath}.exe`)
+
     // Add Nuget.exe CLI tool to path for
     // Other steps to be able to access it
-    const fullPath = `${nugetPath}\\nuget.exe`
+    const fullPath = `${nugetPath}.exe`
     core.debug(`Fullpath ${fullPath}`);
 
     await core.addPath(fullPath);
 
     // Verify Nuget installed
-    // await exec.exec(fullPath);
+    await exec.exec(fullPath);
 
   } catch (error) {
     core.setFailed(error.message);
